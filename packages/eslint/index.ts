b2537b06a1890e82473499163f364189c4657bee
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-12-01 23:53:47 +0900

    📦 eslintを更新

--- packages/eslint/index.ts
+++ packages/eslint/index.ts
@@ -1,24 +1,38 @@
 import eslint from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
+import globals from 'globals';
 import type { Linter } from 'eslint';
 
 export default [
-  eslint.configs.recommended,
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    languageOptions: {
-      parser: tsparser,
-      parserOptions: {
-        project: true,
-      },
-    },
-    plugins: {
-      '@typescript-eslint': tseslint,
-    },
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    },
-  },
+	eslint.configs.recommended,
+	{
+		files: ['**/*.ts', '**/*.tsx'],
+		languageOptions: {
+			parser: tsparser,
+			parserOptions: {
+				project: true,
+			},
+			globals: {
+				...globals.browser,
+				...globals.node,
+				...globals.es2021,
+				Bun: 'readonly',
+			},
+		},
+		plugins: {
+			'@typescript-eslint': tseslint,
+		},
+		rules: {
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-explicit-any': 'error',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+				},
+			],
+		},
+	},
 ] as Linter.Config[];
