73d76eb8e669d883f7802f81e20ca722d24a27aa
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-12-01 23:53:22 +0900

    🎨 tokenRepositoryのインターフェースを定義

--- apps/api/src/domain/repository/tokenRepository.ts
+++ apps/api/src/domain/repository/tokenRepository.ts
@@ -1,5 +1,5 @@
 export interface ITokenRepository {
-  saveRefreshToken(userId: string, token: string): Promise<void>;
-  findRefreshToken(userId: string): Promise<string | null>;
-  deleteRefreshToken(userId: string): Promise<void>;
+	saveRefreshToken(_userId: string, _token: string): Promise<void>;
+	findRefreshToken(userId: string): Promise<string | null>;
+	deleteRefreshToken(userId: string): Promise<void>;
 }
