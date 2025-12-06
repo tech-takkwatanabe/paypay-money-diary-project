551f095b1a654b50f73d70bcd32eda8c62752dd4
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-12-06 23:49:36 +0900

    🎨 フォーマット実行

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,11 +1,11 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './schema';
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
 
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
-  throw new Error('DATABASE_URL is not defined');
+  throw new Error("DATABASE_URL is not defined");
 }
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
