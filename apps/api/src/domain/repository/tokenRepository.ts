2806077eb16b75ff35d516415cbe431c6e5b1c41
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-12-06 23:50:10 +0900

    🎨 フォーマット実行

--- apps/api/src/domain/repository/tokenRepository.ts
+++ apps/api/src/domain/repository/tokenRepository.ts
@@ -1,5 +1,5 @@
 export interface ITokenRepository {
-	saveRefreshToken(_userId: string, _token: string): Promise<void>;
-	findRefreshToken(userId: string): Promise<string | null>;
-	deleteRefreshToken(userId: string): Promise<void>;
+  saveRefreshToken(_userId: string, _token: string): Promise<void>;
+  findRefreshToken(userId: string): Promise<string | null>;
+  deleteRefreshToken(userId: string): Promise<void>;
 }
