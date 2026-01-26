1cf690ee1548c5b59aca745c0df5c29a4c871b42
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2026-01-27 01:22:48 +0900

    🎨 #14 updateTransactionUseCaseの修正

--- apps/api/src/usecase/transaction/updateTransactionUseCase.ts
+++ apps/api/src/usecase/transaction/updateTransactionUseCase.ts
@@ -17,6 +17,11 @@ export class UpdateTransactionUseCase {
       throw new Error("Forbidden: Only amount of cash transactions can be updated");
     }
 
+    // 店名・内容を更新する場合、手動のみ許可
+    if (input.description !== undefined && transaction.paymentMethod !== "手動") {
+      throw new Error("Forbidden: Only description of cash transactions can be updated");
+    }
+
     const updatedTransaction = await this.transactionRepository.update(id, input);
     return updatedTransaction.toResponse();
   }
