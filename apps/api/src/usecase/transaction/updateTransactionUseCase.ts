4cb8860c68a32754bf623cc8a8185e3ce88ce889
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2026-01-31 09:05:28 +0900

    :necktie: #16 手動支出のみ日付と店名を更新可能にするバリデーションを追加

--- apps/api/src/usecase/transaction/updateTransactionUseCase.ts
+++ apps/api/src/usecase/transaction/updateTransactionUseCase.ts
@@ -22,6 +22,11 @@ export class UpdateTransactionUseCase {
       throw new Error("Forbidden: Only description of cash transactions can be updated");
     }
 
+    // 日付を更新する場合、手動のみ許可
+    if (input.date !== undefined && transaction.paymentMethod !== "手動") {
+      throw new Error("Forbidden: Only date of cash transactions can be updated");
+    }
+
     const updatedTransaction = await this.transactionRepository.update(id, input);
     return updatedTransaction.toResponse();
   }
